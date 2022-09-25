b10705325bcf9bf07e59705b9998bccf420a20c9
Olin44 <email@email.com>
2022-09-25 15:30:33 +0200

    modify first example

--- src/main/java/tasks/streams/StreamTasksExample.java
+++ src/main/java/tasks/streams/StreamTasksExample.java
@@ -1,7 +1,12 @@
 package tasks.streams;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
+@Profile("example")
 class StreamTasksExample implements StreamTasks {
     @Override
     public List<String> filterDistinctString(List<String> notDistinctStrings) {
