66416edd3a204b53fa0e9f386f2317cd810a2567
Olin44 <email@email.com>
2022-09-25 15:39:13 +0200

    add tests in first task

--- src/test/java/tasks/streams/StreamTasksTest.java
+++ src/test/java/tasks/streams/StreamTasksTest.java
@@ -1,10 +1,15 @@
 package tasks.streams;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StreamTasksTest {
@@ -12,8 +17,23 @@ class StreamTasksTest {
     @Autowired
     StreamTasks streamTasks;
 
-    @Test
-    void name() {
-        streamTasks.filterDistinctString(List.of());
+    @ParameterizedTest
+    @MethodSource("argumentsForShouldReturnDistinctStrings")
+    void shouldReturnDistinctStrings(List<String> notDistinctStrings, List<String> distinctStrings) {
+        //when
+        List<String> providedDistinctString = streamTasks.filterDistinctString(notDistinctStrings);
+        //then
+        assertEquals(providedDistinctString, distinctStrings);
+    }
+
+    private static Stream<Arguments> argumentsForShouldReturnDistinctStrings() {
+        return Stream.of(
+                Arguments.of(List.of(), List.of()),
+                Arguments.of(List.of("first"), List.of("first")),
+                Arguments.of(List.of("first", "first", "first"), List.of("first")),
+                Arguments.of(List.of("first", "second", "first"), List.of("first", "second")),
+                Arguments.of(List.of("first", "second", "first", "second"), List.of("first", "second")),
+                Arguments.of(List.of("first", "second", "first", "second"), List.of("first", "second"))
+        );
     }
 }
