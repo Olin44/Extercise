0965619cc1777c499f3fe9511c07771b952a9016
Olin44 <email@email.com>
2022-09-25 17:45:38 +0200

    added filter test

--- src/main/java/tasks/streams/StreamTasksExample.java
+++ src/main/java/tasks/streams/StreamTasksExample.java
@@ -3,6 +3,7 @@ package tasks.streams;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -20,6 +21,19 @@ class StreamTasksExample implements StreamTasks {
     public List<Integer> reverseOrder(List<Integer> integers) {
         return integers.stream()
                 .sorted(Comparator.reverseOrder())
+                .toList();
+    }
+
+    @Override
+    public List<String> dnaToListOfTriplets(String dna) {
+        return Arrays.stream(dna.split("(?<=\\G.{3})"))
+                .map(String::toUpperCase)
+                .toList();
+    }
+
+    public List<Integer> removeGreaterThanProvidedMaxNumber(List<Integer> integers, int maxNumber) {
+        return integers.stream()
+                .filter(integer -> integer < maxNumber)
                 .toList();
     }
 }
